35eebc42b6793fb709510d3db7d150f4361e3b67
jake <loncerasjakerei@gmail.com>
2022-05-18 15:16:39 +0800

    modified devise signup to include account addional information such as names

--- stock-trading-backend/app/controllers/application_controller.rb
+++ stock-trading-backend/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
+# ApplicationController's Template
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer
+      .permit(
+        :sign_up,
+        keys: %i[first_name last_name user_name]
+      )
+  end
 end
--- stock-trading-backend/db/schema.rb
+++ stock-trading-backend/db/schema.rb
@@ -10,19 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_518_064_906) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_070145) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'accounts', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_accounts_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_accounts_on_reset_password_token', unique: true
+  create_table "accounts", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
+
 end

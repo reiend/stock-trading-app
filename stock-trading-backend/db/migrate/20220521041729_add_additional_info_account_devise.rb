e1db6488b65c0722de0241b12abfcb74a47cdd84
jake <loncerasjakerei@gmail.com>
2022-05-22 21:32:27 +0800

    modified migration account set role trader by default

--- stock-trading-backend/db/migrate/20220521041729_add_additional_info_account_devise.rb
+++ stock-trading-backend/db/migrate/20220521041729_add_additional_info_account_devise.rb
@@ -1,7 +1,7 @@
 class AddAdditionalInfoAccountDevise < ActiveRecord::Migration[7.0]
   def change
-    add_column :account, :role, :string, :default: "trader"
-    add_column :account, :first_name, :string, 
-    add_column :account, :last_name, :string 
+    add_column :accounts, :role, :string, :default: "trader"
+    add_column :accounts, :first_name, :string, 
+    add_column :accounts, :last_name, :string 
   end
 end

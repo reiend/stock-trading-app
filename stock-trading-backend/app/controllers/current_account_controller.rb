757c899c024a03bf5c20f337308a4df9458e125c
jake <loncerasjakerei@gmail.com>
2022-05-23 16:22:46 +0800

    fix mixing host on when doing running rspec

---
+++ stock-trading-backend/app/controllers/current_account_controller.rb
@@ -0,0 +1,2 @@
+class CurrentAccountController < ApplicationController
+end
---
+++ stock-trading-backend/spec/requests/current_account_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'CurrentAccounts', type: :request do
+  # describe 'Get' do
+  #   before(:each) do
+  #     get '/current_acount'
+  #   end
+  #   describe '/current_account' do
+  #     it 'success response' do
+  #       expect(response).to have_http_status(:success)
+  #     end
+  #     it 'response json' do
+  #       expect(response.content_type).to eq('application/json; charset=utf-8')
+  #     end
+  #   end
+  # end
+end

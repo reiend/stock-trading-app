3b60606beff2bbc5933989f561854b792766a1f3
jake <loncerasjakerei@gmail.com>
2022-05-24 19:56:26 +0800

    🧹 Clean ups

--- stock-trading-backend/spec/request/current_account_spec.rb
+++ stock-trading-backend/spec/request/current_account_spec.rb
@@ -2,16 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'CurrentAccounts', type: :request do
   describe 'Get' do
-    describe 'Admin and Trader' do
-      before do
-        @account = FactoryBot.create :account
+    describe 'Trader' do
+      before(:each) do
+        @account = FactoryBot.create :trader
         @account.confirm
         sign_in @account
+        get '/current_account'
       end
       describe '/current_account' do
-        before(:each) do
-          get '/current_account'
-        end
         it 'success response' do
           expect(response).to have_http_status(:success)
         end
@@ -19,9 +17,6 @@ RSpec.describe 'CurrentAccounts', type: :request do
           expect(response.content_type).to eq('application/json; charset=utf-8')
         end
       end
-    end
-
-    describe "Trader" do
       describe '/current_account/stocks_bought' do
         before(:each) do
           get '/current_account/stocks_bought'
@@ -56,7 +51,13 @@ RSpec.describe 'CurrentAccounts', type: :request do
         end
       end
     end
-    describe "Admin" do
+    describe 'Admin' do
+      before(:each) do
+        @account = FactoryBot.create :admin
+        @account.confirm
+        sign_in @account
+        get '/current_account'
+      end
       describe '/current_account/trader_list' do
         before(:each) do
           get '/current_account/trader_list'
@@ -68,7 +69,6 @@ RSpec.describe 'CurrentAccounts', type: :request do
           expect(response.content_type).to eq('application/json; charset=utf-8')
         end
       end
-
     end
   end
 end

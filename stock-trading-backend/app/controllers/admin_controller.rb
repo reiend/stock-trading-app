222e68f3d7b743b90075c0dd3ec57326dc1da73c
jake <loncerasjakerei@gmail.com>
2022-05-26 15:13:14 +0800

    added auth for admin controller

--- stock-trading-backend/app/controllers/admin_controller.rb
+++ stock-trading-backend/app/controllers/admin_controller.rb
@@ -1,2 +1,10 @@
 class AdminController < ApplicationController
+  before_action :authenticate_account!
+  before_action :admin?
+
+  private
+
+  def admin?
+    current_account.role == 'admin'
+  end
 end

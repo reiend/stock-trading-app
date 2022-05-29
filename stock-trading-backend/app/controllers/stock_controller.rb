f1514114642e7e51a52e2d0522d484a56a1ae704
jake <loncerasjakerei@gmail.com>
2022-05-29 20:42:41 +0800

    added request for viewing stock list

--- stock-trading-backend/app/controllers/stock_controller.rb
+++ stock-trading-backend/app/controllers/stock_controller.rb
@@ -1,2 +1,11 @@
 class StockController < ApplicationController
+  def list
+    @stock_list = Stock.all
+
+    render json: {
+      status: '200',
+      message: 'successfully view stock list',
+      stock_list: @stock_list
+    }
+  end
 end

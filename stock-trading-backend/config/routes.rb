7ca832230af8c19bb1485730a3c46689c55ab7b8
jake <loncerasjakerei@gmail.com>
2022-05-30 23:17:08 +0800

    Fix buy and sell endpoints not working properly

--- stock-trading-backend/config/routes.rb
+++ stock-trading-backend/config/routes.rb
@@ -11,13 +11,12 @@ Rails.application.routes.draw do
 
   get '/current_account', to: 'current_account#index'
   get '/current_account/stocks_bought', to: 'current_account#stocks_bought'
-  get '/current_account/stocks_sold', to: 'current_account#stocks_bought'
+  get '/current_account/stocks_sold', to: 'current_account#stocks_sold'
   get '/current_account/transactions', to: 'current_account#transactions'
 
   get '/current_account/trader_list', to: 'current_account#trader_list'
 
   post '/admin/trader/create', to: 'admin#create'
-
   patch '/admin/trader/:id/approve', to: 'admin#approve'
   get '/admin/trader', to: 'admin#trader'
   get '/admin/traders/pending_approve', to: 'admin#traders_pending'
@@ -26,6 +25,4 @@ Rails.application.routes.draw do
   post '/trader/sell', to: 'trader#sell'
 
   get '/stock_list', to: 'stock#list'
-
-
 end
